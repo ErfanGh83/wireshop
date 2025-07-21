9a32196975b983967d7aadfe86cbd58326894887
ErfanGh <erfan.ghasemian83@gmail.com>
2025-07-21 21:22:46 +0330

    style: improved overall ui

--- src/components/headers/MainHeader.tsx
+++ src/components/headers/MainHeader.tsx
@@ -8,25 +8,41 @@ const MainHeader = () => {
 
     return (
         <div
-            className='w-screen h-12 md:h-14 xl:h-16 flex flex-row-reverse items-center justify-between px-4 border-b-2 border-gray-100'
+            className='w-screen h-12 md:h-14 xl:h-16 flex flex-row-reverse items-center justify-between px-4 border-b-[4px] border-gray-100'
         >
 
-            <Link
-                href={'/'}
-                className='w-48 h-10 flex flex-row-reverse items-center p-1 rounded-sm bg-white hover:border-blue-400 hover:text-blue-500 border-[2px] transition-all'
+            <div
+                className='w-52 h-10 flex flex-row-reverse items-center p-1 rounded-sm bg-white border-[1px]'
             >
                 <div
-                    className='size-fit p-1 m-auto'
+                    className='size-fit p-1 m-auto text-blue-500'
                 >
-                    <FaUser size={18}/>
+                    <FaUser size={18} />
                 </div>
 
                 <div
-                    className='size-full flex items-center justify-center text-lg font-normal'
+                    className='size-full flex items-center justify-center gap-1 text-lg font-normal'
                 >
-                    <p>ورود / ثبت نام</p>
+                    <Link
+                        href={`/login`}
+                        className='size-full flex items-center justify-center pb-2 hover:text-blue-500 transition-colors'
+                    >
+                        <p>ورود</p>
+                    </Link>
+
+                    <div
+                        className='h-full border-l border-[1px]'
+                    >
+                    </div>
+
+                    <Link
+                        href={`/sign-up`}
+                        className='size-full items-center justify-center mx-3 hover:text-blue-500 transition-colors'
+                    >
+                        <p>ثبت نام</p>
+                    </Link>
                 </div>
-            </Link>
+            </div>
 
             <div
                 className='w-full flex items-center justify-center'
