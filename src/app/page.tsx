273e58b74c9f1a8f707d23acad2b8e3115c45964
ErfanGh <erfan.ghasemian83@gmail.com>
2025-07-21 16:27:56 +0330

    feat: added theme switch test to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ export default function Home() {
 
   return (
     <div
-      className="w-screen h-screen bg-white text-black"
+      className="w-screen h-screen bg-white dark:bg-black text-black dark:text-white"
     >
       <ThemeSwitchButton />
+      خانه
     </div>
   );
 }
