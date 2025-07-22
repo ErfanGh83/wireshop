87f18156b8f277455de086633fe6e8ef792499c0
ErfanGh <erfan.ghasemian83@gmail.com>
2025-07-22 10:38:29 +0330

    style: made the main header responsive

---
+++ src/components/buttons/SignupOrLoginButton.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import React from 'react'
+import { FaUser } from 'react-icons/fa'
+
+const SignupOrLoginButton = () => {
+    return (
+        <div
+            className='w-32 md:w-40 xl:w-44 h-8 md:h-10 flex flex-row-reverse items-center p-1 rounded-sm bg-white border-[1px]'
+        >
+            <div
+                className='hidden xl:block md:size-fit p-1 m-auto text-blue-400'
+            >
+                <FaUser size={18} />
+            </div>
+
+            <div
+                className='size-full flex items-center justify-center gap-1 text-sm md:text-lg font-normal'
+            >
+                <Link
+                    href={`/login`}
+                    className='size-full flex items-center justify-center md:pb-1 hover:text-blue-400 transition-colors'
+                >
+                    <p>ورود</p>
+                </Link>
+
+                <div
+                    className='h-full border-l border-[1px]'
+                >
+                </div>
+
+                <Link
+                    href={`/sign-up`}
+                    className='size-full items-center justify-center pt-1 md:pt-0 mr-2 md:mx-3 hover:text-blue-400 transition-colors'
+                >
+                    <p>ثبت نام</p>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default SignupOrLoginButton
--- src/components/headers/MainHeader.tsx
+++ src/components/headers/MainHeader.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import SearchBar from './main-header-components/SearchBar'
-import { FaUser } from 'react-icons/fa'
-import Link from 'next/link'
 import HamburgerMenuSection from './main-header-components/HamburgerMenuSection'
+import SignupOrLoginButton from '../buttons/SignupOrLoginButton'
 
 const MainHeader = () => {
 
@@ -11,37 +10,11 @@ const MainHeader = () => {
             className='w-screen h-12 md:h-14 xl:h-16 flex flex-row-reverse items-center justify-between px-4 border-b-[4px] border-gray-100'
         >
 
+
             <div
-                className='w-52 h-10 flex flex-row-reverse items-center p-1 rounded-sm bg-white border-[1px]'
+                className='size-fit'
             >
-                <div
-                    className='size-fit p-1 m-auto text-blue-400'
-                >
-                    <FaUser size={18} />
-                </div>
-
-                <div
-                    className='size-full flex items-center justify-center gap-1 text-lg font-normal'
-                >
-                    <Link
-                        href={`/login`}
-                        className='size-full flex items-center justify-center pb-1 hover:text-blue-400 transition-colors'
-                    >
-                        <p>ورود</p>
-                    </Link>
-
-                    <div
-                        className='h-full border-l border-[1px]'
-                    >
-                    </div>
-
-                    <Link
-                        href={`/sign-up`}
-                        className='size-full items-center justify-center mx-3 hover:text-blue-400 transition-colors'
-                    >
-                        <p>ثبت نام</p>
-                    </Link>
-                </div>
+                <SignupOrLoginButton />
             </div>
 
             <div
