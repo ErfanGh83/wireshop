79460c2da0ff868b53141850b5d6a67cbdb74be2
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-07-22 19:15:46 +0430

    style: page component got responsive

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -55,15 +55,15 @@ const specs: {
 export default function page() {
   return (
     <MainLayout>
-      <div className=" overflow-y-auto size-full bg-blue-100 dark:bg-slate-500 dark:text-gray-100 flex justify-center items-center p-6 overflow-auto">
-        <div className="bg-white dark:bg-slate-600 shadow-xl rounded-2xl p-6 w-full h-full grid md:grid-cols-6 gap-6">
+      <div className=" overflow-y-auto size-full bg-blue-100 dark:bg-slate-500 dark:text-gray-100 flex justify-center items-center md:p-6 p-2 overflow-auto">
+        <div className="bg-white dark:bg-slate-600 shadow-xl overflow-y-auto md:overflow-y-hidden rounded-2xl md:p-6 p-4 w-full h-full grid grid-cols-1 md:grid-cols-5 lg:grid-cols-6 gap-6">
           {/* Product Image */}
-          <div className="flex justify-center items-center col-span-2">
+          <div className="flex justify-center items-center md:col-span-2">
             <ProductImageSlider images={image_list} discount={10} isFeatured />
           </div>
 
           {/* Product Details */}
-          <div className="flex flex-col justify-start space-y-4 col-span-4 overflow-y-auto pr-1 pt-10">
+          <div className="flex flex-col justify-start space-y-4 md:col-span-3 lg:col-span-4 md:overflow-y-auto pr-1 pt-10">
             <div className="border-r-6 rounded-md p-2 flex flex-col justify-start space-y-4 col-span-3 border-blue-100">
               <h1 className="text-3xl font-bold">نام محصول</h1>
               <p className="text-xl font-semibold">
@@ -91,7 +91,7 @@ export default function page() {
             {/* item Sepcs */}
             <div className="mt-6">
               <h2 className="text-lg font-semibold mb-4">مشخصات کابل</h2>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-3">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 px-3">
                 {specs.map((item) => (
                   <ProductDetailSpec
                     label={item.label}
