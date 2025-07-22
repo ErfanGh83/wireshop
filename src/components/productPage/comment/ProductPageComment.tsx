64694b5b41d18ec91cc6f7095ae19ea4e36434fd
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-07-22 19:30:34 +0430

    style: product page comment section dark mode fixed

--- src/components/productPage/comment/ProductPageComment.tsx
+++ src/components/productPage/comment/ProductPageComment.tsx
@@ -8,12 +8,12 @@ interface Props {
 function ProductPageComment({ text, userName }: Props) {
   return (
     <li className="p-2 m-1 grid grid-cols-10 lg:grid-cols-12 xl:grid-cols-15">
-      <div className="p-3 w-10 mt-1 h-10 bg-blue-200 dark:bg-blue-900 rounded-full col-span-1">
+      <div className="p-3 w-10 mt-1 h-10 bg-blue-200 dark: dark:bg-slate-900 rounded-full col-span-1">
         <FaRegUser className=" text-black dark:text-white" />
       </div>
-      <div className="lg:col-span-11 xl:col-span-14 mr-2 col-span-9 bg-blue-50 text-black rounded-xl p-3">
+      <div className="lg:col-span-11 xl:col-span-14 mr-2 col-span-9 bg-blue-50 dark:bg-slate-800/80 dark:text-white text-black rounded-xl p-3">
         <h4 className="text-lg font-bold">{userName}</h4>
-        <p className="text-cyan-800">{text}</p>
+        <p className="text-cyan-800 dark:text-gray-300/80">{text}</p>
       </div>
     </li>
   );
