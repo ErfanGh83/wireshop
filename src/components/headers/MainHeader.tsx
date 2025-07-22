b5b1debf5cd61a255148db9f2ceff90ec04bc5a1
ErfanGh <erfan.ghasemian83@gmail.com>
2025-07-22 12:13:11 +0330

    feat: ultimate responsiveness for searchbar

--- src/components/headers/MainHeader.tsx
+++ src/components/headers/MainHeader.tsx
@@ -4,32 +4,30 @@ import HamburgerMenuSection from './main-header-components/HamburgerMenuSection'
 import SignupOrLoginButton from '../buttons/SignupOrLoginButton'
 
 const MainHeader = () => {
-
-    return (
-        <div
-            className='w-screen h-12 md:h-14 xl:h-16 flex flex-row-reverse items-center justify-between px-4 border-b-[4px] border-gray-100'
-        >
-
-
-            <div
-                className='size-fit'
-            >
-                <SignupOrLoginButton />
-            </div>
-
-            <div
-                className='w-full flex items-center justify-center'
-            >
-                <SearchBar />
-            </div>
-
-            <div
-                className='size-fit flex flex-row-reverse items-center gap-4'
-            >
-                <HamburgerMenuSection />
-            </div>
+  return (
+    <>
+      {/* Main Header Row */}
+      <div className='w-screen h-12 md:h-14 xl:h-16 flex flex-row-reverse items-center justify-between px-4 border-b-[4px] border-gray-100'>
+        <div className='size-fit'>
+          <SignupOrLoginButton />
         </div>
-    )
+
+        {/* Search Bar - Hidden on very small screens */}
+        <div className='hidden min-[460px]:block w-full items-center justify-center'>
+          <SearchBar />
+        </div>
+
+        <div className='size-fit flex flex-row-reverse items-center gap-4'>
+          <HamburgerMenuSection />
+        </div>
+      </div>
+
+      {/* Additional Search Bar - Only shown on very small screens */}
+      <div className='min-[460px]:hidden w-full px-4 py-2 border-b border-gray-100'>
+        <SearchBar />
+      </div>
+    </>
+  )
 }
 
 export default MainHeader
