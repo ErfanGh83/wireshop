0b3d441dedcc048878e8cbbfb39bf74f64827f8c
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-07-22 19:23:24 +0430

    style: product page comment section got responsive

--- src/components/productPage/comment/ProductPageComment.tsx
+++ src/components/productPage/comment/ProductPageComment.tsx
@@ -7,11 +7,11 @@ interface Props {
 
 function ProductPageComment({ text, userName }: Props) {
   return (
-    <li className="p-3 m-2 grid grid-cols-15">
+    <li className="p-2 m-1 grid grid-cols-10 lg:grid-cols-12 xl:grid-cols-15">
       <div className="p-3 w-10 mt-1 h-10 bg-blue-200 dark:bg-blue-900 rounded-full col-span-1">
         <FaRegUser className=" text-black dark:text-white" />
       </div>
-      <div className="col-span-14 bg-blue-50 text-black rounded-xl p-3">
+      <div className="lg:col-span-11 xl:col-span-14 mr-2 col-span-9 bg-blue-50 text-black rounded-xl p-3">
         <h4 className="text-lg font-bold">{userName}</h4>
         <p className="text-cyan-800">{text}</p>
       </div>
