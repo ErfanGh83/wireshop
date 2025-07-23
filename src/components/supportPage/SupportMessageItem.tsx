ce19b60abd8a5dd8db889f569b50b467434e49f3
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-07-23 14:25:11 +0430

    style: improved dark theme styling on support page for textarea

--- src/components/supportPage/SupportMessageItem.tsx
+++ src/components/supportPage/SupportMessageItem.tsx
@@ -46,7 +46,9 @@ function SupportMessageItem({
           defaultValue={repliedMessage}
           disabled={status == "replied"}
           placeholder="پاسخ شما..."
-          className="w-full mt-2 px-3 py-2 rounded-lg border border-gray-300 dark:border-gray-500 dark:bg-slate-600 dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-400 text-sm resize-none min-h-[80px]"
+          className={`${
+            status == "replied" && "cursor-not-allowed"
+          } w-full mt-2 px-3 py-2 rounded-lg border border-gray-300 dark:border-gray-500 dark:bg-slate-600 dark:text-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-purple-500 text-sm resize-none min-h-[80px]`}
         />
       )}
 
