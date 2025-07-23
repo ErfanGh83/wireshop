8d13c15d50616dc8450a5826587b08ca05f95094
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-07-23 14:13:54 +0430

    style: fixed styling bug in floating chat icon

--- src/components/chat/FloatingChat.tsx
+++ src/components/chat/FloatingChat.tsx
@@ -16,7 +16,7 @@ export default function FloatingChat() {
           isOpen
             ? " bg-blue-400 hover:bg-blue-500 dark:hover:bg-slate-800 dark:bg-slate-700 "
             : " bg-blue-100 hover:bg-blue-200 dark:hover:bg-slate-700 dark:bg-slate-500 "
-        } fixed bottom-8 right-8 z-18 transition-all cursor-pointer text-black p-4 rounded-full shadow-lg dark:text-gray-100`}
+        } fixed bottom-8 right-8 z-55 transition-all cursor-pointer text-black p-4 rounded-full shadow-lg dark:text-gray-100`}
       >
         <FaCommentDots className="w-6 h-6" />
       </button>
@@ -29,7 +29,7 @@ export default function FloatingChat() {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              className="fixed bottom-0 right-0 left-0 top-0 bg-black/50 dark:bg-black/80 z-15"
+              className="fixed bottom-0 right-0 left-0 top-0 bg-black/50 dark:bg-black/80 z-45"
               onClick={() => setIsOpen(false)}
             />
 
@@ -38,11 +38,11 @@ export default function FloatingChat() {
               initial={{ y: 30, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ ease: "easeOut" }}
-              className="fixed bottom-24 right-6 w-80 md:w-120 max-w-full h-120 bg-white dark:bg-slate-700 text-black dark:text-gray-100 rounded-2xl shadow-2xl z-17 flex flex-col overflow-hidden"
+              className="fixed bottom-24 right-6 w-70 md:w-120 max-w-full h-120 bg-white dark:bg-slate-700 text-black dark:text-gray-100 rounded-2xl shadow-2xl z-50 flex flex-col overflow-hidden"
             >
               <div className="flex justify-between items-center p-4 border-b border-gray-300 dark:border-gray-600">
                 <h2 className="font-semibold">پشتیبانی</h2>
-                <button onClick={() => setIsOpen(false)}>
+                <button className="cursor-pointer hover:scale-120 transition-all" onClick={() => setIsOpen(false)}>
                   <IoClose className="w-5 h-5" />
                 </button>
               </div>
