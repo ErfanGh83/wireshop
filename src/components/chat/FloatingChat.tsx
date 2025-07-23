ef43a6f21926581d454b8dc1eb1b4a4ac8bef3ef
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-07-23 14:30:32 +0430

    style: improved dark theme styling on floating chat

--- src/components/chat/FloatingChat.tsx
+++ src/components/chat/FloatingChat.tsx
@@ -14,8 +14,8 @@ export default function FloatingChat() {
         onClick={() => setIsOpen((prev) => !prev)}
         className={`${
           isOpen
-            ? " bg-blue-400 hover:bg-blue-500 dark:hover:bg-slate-800 dark:bg-slate-700 "
-            : " bg-blue-100 hover:bg-blue-200 dark:hover:bg-slate-700 dark:bg-slate-500 "
+            ? " bg-blue-400 hover:bg-blue-500 dark:hover:bg-purple-400 dark:bg-purple-600 dark:text-gray-300"
+            : " bg-blue-100 hover:bg-blue-200 dark:hover:bg-purple-400 dark:bg-purple-500 dark:text-gray-300"
         } fixed bottom-8 right-8 z-55 transition-all cursor-pointer text-black p-4 rounded-full shadow-lg dark:text-gray-100`}
       >
         <FaCommentDots className="w-6 h-6" />
@@ -42,7 +42,10 @@ export default function FloatingChat() {
             >
               <div className="flex justify-between items-center p-4 border-b border-gray-300 dark:border-gray-600">
                 <h2 className="font-semibold">پشتیبانی</h2>
-                <button className="cursor-pointer hover:scale-120 transition-all" onClick={() => setIsOpen(false)}>
+                <button
+                  className="cursor-pointer hover:scale-120 transition-all"
+                  onClick={() => setIsOpen(false)}
+                >
                   <IoClose className="w-5 h-5" />
                 </button>
               </div>
@@ -63,9 +66,9 @@ export default function FloatingChat() {
                       e.currentTarget.style.height = "auto";
                       e.currentTarget.style.height = `${e.currentTarget.scrollHeight}px`;
                     }}
-                    className="flex-1 px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 dark:bg-slate-600 dark:border-gray-500 resize-none max-h-32 leading-6 overflow-auto"
+                    className="flex-1 px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-purple-400 dark:bg-slate-600 dark:border-gray-500 resize-none max-h-32 leading-6 overflow-auto"
                   />
-                  <button className="bg-blue-100 hover:bg-blue-200 px-4 mb-auto py-2 rounded-lg text-black dark:bg-slate-500 dark:text-gray-100">
+                  <button className="bg-blue-100 hover:bg-blue-200 hover:dark:bg-purple-300 cursor-pointer transition-all px-4 mb-auto py-2 rounded-lg text-black dark:bg-slate-500 dark:text-gray-100">
                     ارسال
                   </button>
                 </div>
