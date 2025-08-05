2999e39ecb6e8a0290864cb258d98abcdfe210ac
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-08-05 21:20:12 +0430

    feat: added delete request function

--- src/lib/api/cartApi.ts
+++ src/lib/api/cartApi.ts
@@ -1,7 +1,15 @@
 import { Cart } from "@/types/cart";
-import { get } from "./apiClient";
+import { del, get, post } from "./apiClient";
 import { API_ENDPOINTS } from "./constants";
 
 export async function getAllCart(): Promise<Cart> {
   return get<Cart>(API_ENDPOINTS.MY_CONVERSATION);
+}
+
+export async function addCartItem(id: string, quantity: number) {
+  return post(`${API_ENDPOINTS.MY_CONVERSATION}/${id}`, { quantity });
+}
+
+export async function removeCartItem(id: string, quantity: number) {
+  return del(`${API_ENDPOINTS.MY_CONVERSATION}/${id}`, { quantity });
 }
