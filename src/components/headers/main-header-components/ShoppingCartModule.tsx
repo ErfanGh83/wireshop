fb6da882b02458c30dd00c3b147a878497693c03
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-08-07 10:50:57 +0430

    feat: added loading and error for shopping cart modal

--- src/components/headers/main-header-components/ShoppingCartModule.tsx
+++ src/components/headers/main-header-components/ShoppingCartModule.tsx
@@ -5,6 +5,7 @@ import { FaXmark } from "react-icons/fa6";
 import ShoppingCartItem from "./ShoppingCartItem";
 import { getAllCart } from "@/lib/api/cartApi";
 import { Cart } from "@/types/cart";
+import Spinner from "@/components/spinner/Spinner";
 
 type Props = {
   setModuleIsOpen: Dispatch<SetStateAction<boolean>>;
@@ -13,11 +14,14 @@ type Props = {
 
 const ShoppingCartModule = ({ setModuleIsOpen, moduleIsOpen }: Props) => {
   const [cartItems, setCartItems] = useState<Cart | undefined>();
+  const [err, setErr] = useState<string>("");
 
   useEffect(() => {
     if (!moduleIsOpen) return;
 
-    getAllCart().then((res) => setCartItems(res));
+    getAllCart()
+      .then((res) => setCartItems(res))
+      .catch((error) => setErr(error.message));
   }, [moduleIsOpen]);
 
   const handleCloseModule = () => {
@@ -37,28 +41,37 @@ const ShoppingCartModule = ({ setModuleIsOpen, moduleIsOpen }: Props) => {
         <div className="w-full h-10 flex items-center justify-center">
           سبد خرید
         </div>
+        {cartItems ? (
+          <>
+            <div className="size-full flex flex-col gap-2 overflow-y-auto p-2 bg-gray-100 dark:bg-slate-800">
+              {cartItems &&
+                cartItems.items.map((item, index) => (
+                  <ShoppingCartItem
+                    key={index}
+                    title={item.product.name}
+                    quantity={item.quantity}
+                    price={item.product.price}
+                    discount={0}
+                    total={item.quantity * item.product.price}
+                    itemId={item.id}
+                    productId={item.product.id}
+                  />
+                ))}
+            </div>
 
-        <div className="size-full flex flex-col gap-2 overflow-y-auto p-2 bg-gray-100 dark:bg-slate-800">
-          {cartItems &&
-            cartItems.items.map((item, index) => (
-              <ShoppingCartItem
-                key={index}
-                title={item.product.name}
-                quantity={item.quantity}
-                price={item.product.price}
-                discount={0}
-                total={item.quantity * item.product.price}
-                itemId={item.id}
-                productId={item.product.id}
-              />
-            ))}
-        </div>
+            <div className="w-full h-12 flex items-center justify-center">
+              <button className="size-full bg-blue-500 text-white cursor-pointer hover:bg-blue-600 transition-colors">
+                تسویه حساب
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="mb-[70%] text-center">
+            <Spinner size={30} />
+          </div>
+        )}
 
-        <div className="w-full h-12 flex items-center justify-center">
-          <button className="size-full bg-blue-500 text-white cursor-pointer hover:bg-blue-600 transition-colors">
-            تسویه حساب
-          </button>
-        </div>
+        {err && <div className="text-red-500 mx-auto my-auto">{err}</div>}
       </div>
     </div>
   );
