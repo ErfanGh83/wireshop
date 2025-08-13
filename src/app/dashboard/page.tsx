3df4593ccb4fe43bf2a2848a854016e799440862
ErfanGh <erfan.ghasemian83@gmail.com>
2025-08-13 18:54:38 +0330

    style: small ui fix for dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -135,7 +135,7 @@ export default function DashboardPage() {
                                     </div>
 
                                     {/* User Info Cards */}
-                                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
+                                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-6">
                                         <UserInfoContainer
                                             title="آدرس ها"
                                             description="آدرس فعلی: تهران، میدان رسالت ..."
