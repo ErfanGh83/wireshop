2e0f1bdd98445b3b1f623a119b48d0e2ba420f79
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-08-14 14:42:59 +0430

    fix: added suspense to support route

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,12 +1,15 @@
 import MainLayout from "@/components/layouts/MainLayout";
 import SupportMessageContainer from "@/components/supportPage/SupportMessageContainer";
+import { Suspense } from "react";
 
 function page() {
   return (
     <MainLayout>
       <div className=" overflow-y-auto size-full bg-blue-100 dark:bg-slate-500 dark:text-gray-100 flex justify-center items-center md:p-6 p-2 overflow-auto">
         <div className="bg-white dark:bg-slate-600 shadow-xl overflow-y-auto rounded-2xl md:p-6 p-4 w-full h-full">
-          <SupportMessageContainer />
+          <Suspense fallback="در حال بارگذاری...">
+            <SupportMessageContainer />
+          </Suspense>
         </div>
       </div>
     </MainLayout>
