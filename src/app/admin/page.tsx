b1627fd9dbd69461cc2fdd4e954d8d7e9ee45cf4
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-08-14 14:39:07 +0430

    fix: added suspense to admin, cart, product route

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,6 +5,7 @@ import AdminEditProduct from "@/components/adminPage/AdminEditProduct";
 import AdminOrdersList from "@/components/adminPage/AdminOrdersList";
 import AdminTabsContainer from "@/components/adminPage/AdminTabsContainer";
 import MainLayout from "@/components/layouts/MainLayout";
+import { Suspense } from "react";
 
 const adminTabs = [
   {
@@ -26,7 +27,7 @@ const adminTabs = [
   {
     title: "افزودن عضو",
     component: <AdminAddUser />,
-  }
+  },
 ];
 
 export default function Home() {
@@ -34,7 +35,9 @@ export default function Home() {
     <MainLayout>
       <div className=" size-full bg-blue-100 dark:bg-slate-500 dark:text-gray-100 flex justify-center items-center md:p-6 p-2 overflow-hidden">
         <div className="bg-white dark:bg-slate-600 shadow-xl rounded-2xl md:p-4 p-2 w-full h-full overflow-hidden">
-          <AdminTabsContainer contains={adminTabs} />
+          <Suspense fallback="در حال بارگذاری...">
+            <AdminTabsContainer contains={adminTabs} />
+          </Suspense>
         </div>
       </div>
     </MainLayout>
