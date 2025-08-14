403642de2c8a4a65de40071e809f5747976e151e
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2025-08-14 14:35:20 +0430

    fix: added suspense to receipt page

--- src/app/receipt/page.tsx
+++ src/app/receipt/page.tsx
@@ -1,70 +1,16 @@
-"use client";
+import PurchaseReceipt from "@/components/receipt/PurchaseReceipt";
+import { Suspense } from "react";
 
-import { useSearchParams } from "next/navigation";
-import { useMemo } from "react";
-
-export default function PurchaseReceipt() {
-  const searchParams = useSearchParams();
-  const refId = searchParams.get("refId");
-  const success = searchParams.get("success");
-
-  const formattedDate = useMemo(() => {
-    const now = new Date();
-    return now.toLocaleString("fa-IR", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-  }, []);
-
+function page() {
   return (
-    <div className="max-w-md mx-auto bg-white dark:bg-slate-800 rounded-xl shadow-lg p-6 text-center mt-30">
-      <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-200">
-        رسید خرید
-      </h2>
-
-      <div className="my-2">
-        <p className="text-lg">
-          <span className="font-semibold text-gray-700 dark:text-gray-300">
-            شماره مرجع:
-          </span>{" "}
-          {refId ?? "—"}
-        </p>
-        <p className="text-lg">
-          <span className="font-semibold text-gray-700 dark:text-gray-300">
-            وضعیت پرداخت:
-          </span>{" "}
-          {success ? (
-            <span className="text-green-600 font-bold">موفق</span>
-          ) : (
-            <span className="text-red-600 font-bold">ناموفق</span>
-          )}
-        </p>
-        <p className="text-sm text-gray-500 dark:text-gray-400">
-          زمان: {formattedDate}
-        </p>
+    <div className=" overflow-y-auto size-full bg-blue-100 dark:bg-slate-500 dark:text-gray-100 flex justify-center items-center md:p-6 p-2 overflow-auto">
+      <div className="bg-white dark:bg-slate-600 shadow-xl overflow-y-auto md:overflow-y-hidden rounded-2xl md:p-6 p-4 w-full h-full grid grid-cols-1 md:grid-cols-5 lg:grid-cols-6 gap-6">
+        <Suspense fallback="در حال صدور رسید...">
+          <PurchaseReceipt />
+        </Suspense>
       </div>
-
-      {success ? (
-        <>
-          <p className="text-gray-700 dark:text-gray-300">
-            شکیبا باشید تا محصول برایتان ارسال شود.
-          </p>
-          <p className="text-gray-700 dark:text-gray-300">
-            برای پیگیری سفارش به{" "}
-            <a href="/dashboard" className="text-blue-500 underline">
-              داشبورد کاربر
-            </a>{" "}
-            مراجعه کنید.
-          </p>
-        </>
-      ) : (
-        <p className="text-red-500 font-semibold">
-          پرداخت ناموفق بود. لطفا دوباره تلاش کنید.
-        </p>
-      )}
     </div>
   );
 }
+
+export default page;
